e1ac50c7f3cf867c8329064de505de13603bfff1
Najim Mazidi <nmazidi95@gmail.com>
2017-02-01 21:44:20 +0000

    Created projects for desktop, mobile and web applications

---
+++ MobileApplication/MobileApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileApplication
+//
+//  Created by Najim Mazidi on 01/02/2017.
+//  Copyright © 2017 PRCS251-Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
