790fed3011b3ab511de48099e0fc2ce4203a4b3c
Najim Mazidi <nmazidi95@gmail.com>
2017-03-08 17:15:48 +0000

    started mobile ui design

--- MobileApplication/MobileApplication/ViewController.swift
+++ MobileApplication/MobileApplication/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func btnGoLive(_ sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
