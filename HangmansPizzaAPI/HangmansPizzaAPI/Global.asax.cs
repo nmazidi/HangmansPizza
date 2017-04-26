387b75578aacf9f0cc032bf14754405024e9b775
Najim Mazidi <nmazidi95@gmail.com>
2017-04-26 16:33:07 +0100

    Fixed xml error and re published

--- HangmansPizzaAPI/HangmansPizzaAPI/Global.asax.cs
+++ HangmansPizzaAPI/HangmansPizzaAPI/Global.asax.cs
@@ -13,6 +13,8 @@ namespace HangmansPizzaAPI
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
