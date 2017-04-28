f188a0c190cded98d40c77487d43efefb0b7e58a
Najim Mazidi <nmazidi95@gmail.com>
2017-04-28 18:00:57 +0100

    MOB: more GET request stuff

--- MobileApplication/MobileApplication/Data.swift
+++ MobileApplication/MobileApplication/Data.swift
@@ -8,40 +8,52 @@
 
 import Foundation
 
-func GetRequest(){
-//    let url = URL(string : "http://xserve.uopnet.plymouth.ac.uk/Modules/INTPROJ/PRCS251Q/api/")!
-//    let sesh = URLSession(configuration: URLSessionConfiguration.default)
-//    
-//    var request = URLRequest(url: url)
-//    request.httpMethod = "GET"
-//    
-//    let dataTask = sesh.dataTask(with: request as URLRequest) {
-//        (data, response, error) in
-//        if error != nil {
-//            print("an error has occured while calling GET:")
-//            print(error!)
-//            return
-//        }
-//        guard let dataReceived = data
-//            else {
-//                print("Error! No data received")
-//                return
-//        }
-//        do {
-//            guard let jsonData = try JSONSerialization.jsonObject(with: dataReceived, options: []) as? [[String: AnyObject]]
-//                else {
-//                    print("Error! Cannot convert data to JSON")
-//                    return
-//            }
-//            print(isValidCredentials(jsonArray: jsonData))
-//        } catch {
-//            print("Error! Cannot convert data to JSON!")
-//            return
-//        }
-//        
-//        
-//    }
-//    dataTask.resume()
+func GetRequest() {
+    var test = [[String: AnyObject]]()
+    let url = URL(string : "http://xserve.uopnet.plymouth.ac.uk/Modules/INTPROJ/PRCS251Q/api/delivery_rider")!
+    let sesh = URLSession(configuration: URLSessionConfiguration.default)
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+    
+    let dataTask = sesh.dataTask(with: request as URLRequest) {
+        (data, response, error) in
+        if error != nil {
+            print("an error has occured while calling GET:")
+            print(error!)
+            return
+        }
+        guard let dataReceived = data
+            else {
+                print("Error! No data received")
+                return
+        }
+        do {
+            guard let jsonData = try JSONSerialization.jsonObject(with: dataReceived, options: []) as? [[String: AnyObject]]
+                else {
+                    print("Error! Cannot convert data to JSON")
+                    return
+            }
+            //print(jsonData)
+            test = jsonData
+        } catch {
+            print("Error! Cannot convert data to JSON!")
+            return
+        }
+        
+    }
+    dataTask.resume()
+    print(test)
+}
+
+func convertToDictionary(str:String) -> [String: Any]? {
+    if let data = str.data(using: .utf8) {
+        do {
+            return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    return nil
 }
 
     
--- MobileApplication/MobileApplication/ViewController.swift
+++ MobileApplication/MobileApplication/ViewController.swift
@@ -33,43 +33,44 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func btnLoginClicked(_ sender: Any) {
-        UIApplication.shared.isNetworkActivityIndicatorVisible = true
-        
-        let url = URL(string : "http://xserve.uopnet.plymouth.ac.uk/Modules/INTPROJ/PRCS251Q/api/delivery_rider")!
-        let sesh = URLSession(configuration: URLSessionConfiguration.default)
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        let dataTask = sesh.dataTask(with: request as URLRequest) {
-            (data, response, error) in
-            if error != nil {
-                print("an error has occured while calling GET:")
-                print(error!)
-                return
-            }
-            guard let dataReceived = data
-                else {
-                    print("Error! No data received")
-                    return
-                }
-            do {
-                guard let jsonData = try JSONSerialization.jsonObject(with: dataReceived, options: []) as? [[String: AnyObject]]
-                    else {
-                        print("Error! Cannot convert data to JSON")
-                        return
-                    }
-                print(jsonData)
-                print(self.isValidCredentials(jsonArray: jsonData))
-            } catch {
-                print("Error! Cannot convert data to JSON!")
-                return
-            }
-
-            
-        }
-        UIApplication.shared.isNetworkActivityIndicatorVisible = false
-        dataTask.resume()
+//        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+//        
+//        let url = URL(string : "http://xserve.uopnet.plymouth.ac.uk/Modules/INTPROJ/PRCS251Q/api/delivery_rider")!
+//        let sesh = URLSession(configuration: URLSessionConfiguration.default)
+//        
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "GET"
+//        
+//        let dataTask = sesh.dataTask(with: request as URLRequest) {
+//            (data, response, error) in
+//            if error != nil {
+//                print("an error has occured while calling GET:")
+//                print(error!)
+//                return
+//            }
+//            guard let dataReceived = data
+//                else {
+//                    print("Error! No data received")
+//                    return
+//                }
+//            do {
+//                guard let jsonData = try JSONSerialization.jsonObject(with: dataReceived, options: []) as? [[String: AnyObject]]
+//                    else {
+//                        print("Error! Cannot convert data to JSON")
+//                        return
+//                    }
+//                print(jsonData)
+//                print(self.isValidCredentials(jsonArray: jsonData))
+//            } catch {
+//                print("Error! Cannot convert data to JSON!")
+//                return
+//            }
+//
+//            
+//        }
+//        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+//        dataTask.resume()
+        GetRequest()
     }
     func isValidCredentials(jsonArray : [[String: AnyObject]]) -> Bool {
         for item in jsonArray {
@@ -81,7 +82,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
         }
         return false
-    }
 
+    }
+        
 }
 
