c820541048dd042f02f0e93a6ff0b4616de95627
NathanScarfi97 <nathan.scarfi@students.plymouth.ac.uk>
2017-05-04 18:04:20 +0100

    Get Request Implemented, Post still to be done

--- DesktopApplication2/DesktopApp/src/desktopapp/datamodel/APIConnection.java
+++ DesktopApplication2/DesktopApp/src/desktopapp/datamodel/APIConnection.java
@@ -5,10 +5,102 @@
  */
 package desktopapp.datamodel;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  *
  * @author Nathan
  */
 public class APIConnection {
+    
+    public static final String USER_AGENT = "Mozilla/5.0";          
+    public static String url = 
+                    "http://xserve.uopnet.plymouth.ac.uk/modules/intproj/prcs251q/API/customers";
+    
+    //HTTP GET request
+    public void getRequest() throws IOException {
+                        
+            try {
+                
+                URL obj = new URL(url);
+                HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+            
+                //set request method and request header
+                con.setRequestMethod("GET");
+                con.setRequestProperty("User-Agent", USER_AGENT);
+            
+                int responseCode = con.getResponseCode();
+                System.out.println("\nSending 'GET' request to URL : " + url);
+                System.out.println("Response Code : " + responseCode);
+            
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+            
+                while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);            
+                }
+                in.close();
+            
+                String output = response.toString();
+            
+                System.out.println(output);
+                  
+            } catch (IOException e1) {
+                System.out.println("IOException");
+            }           
+            
+    }
+    
+    //HTTP POST request
+    public void postRequest() throws IOException, MalformedURLException, ProtocolException {
+        
+        URL url = new URL("http://xserve.uopnet.plymouth.ac.uk/modules/intproj/prcs251q/API/customers");
+        Map<String,Object> params = new LinkedHashMap<>();
+        params.put("CUSTOMER_ID", 3);
+        params.put("CUSTOMER_TITLE", "Mr");
+        params.put("CUSTOMER_FORENAME", "Nathan");
+        params.put("CUSTOMER_SURNAME", "Scarfi");
+        params.put("CUSTOMER_EMAIL", "nathanscarfi@gmail.com");
+        params.put("CUSTOMER_PHONE", "07565772859");
+        params.put("CUSTOMER_PASSWORD", "password");
+        params.put("CUSTOMER_DOB", "30-MAR-97");
+        
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String,Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        int responseCode = conn.getResponseCode();
+        System.out.println("Response Code : " + responseCode);
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(postDataBytes);
+
+        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+        
+        for (int c; (c = in.read()) >= 0;)
+            System.out.print((char)c); 
+        
+        
+    }
     
 }
--- DesktopApplication2/DesktopApp/src/desktopapp/gui/HomeScreen.java
+++ DesktopApplication2/DesktopApp/src/desktopapp/gui/HomeScreen.java
@@ -4,18 +4,33 @@
  * and open the template in the editor.
  */
 package desktopapp.gui;
-
+import desktopapp.datamodel.APIConnection;
+import java.io.IOException;
 /**
  *
  * @author Nathan
  */
 public class HomeScreen extends javax.swing.JFrame {
 
+    private APIConnection getR;
+    
     /**
      * Creates new form HomeScreen
      */
     public HomeScreen() {
-        initComponents();
+        
+        try {
+            
+            initComponents();
+        
+        
+            getR = new APIConnection();
+        
+            getR.postRequest();
+        } catch (IOException e3) {
+            
+        }
+            
     }
 
     /**
@@ -27,17 +42,47 @@ public class HomeScreen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("Admin");
+        jButton1.setName("adminButton"); // NOI18N
+
+        jButton2.setText("Kitchen");
+        jButton2.setName("kitchenButton"); // NOI18N
+
+        jTextField1.setText("jTextField1");
+        jTextField1.setName("txtTest"); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(82, Short.MAX_VALUE)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 242, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(69, 69, 69)
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton2)))
+                .addGap(76, 76, 76))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(104, 104, 104)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 39, Short.MAX_VALUE)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(36, 36, 36))
         );
 
         pack();
@@ -79,5 +124,8 @@ public class HomeScreen extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
